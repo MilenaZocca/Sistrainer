d717ac5e0e3a2686fd3115c8c4f77d0bab9618c3
unknown <milenaz.skye@gmail.com>
2020-03-25 18:42:32 -0300

    Index.html

---
+++ Relatorio.php
@@ -0,0 +1,11 @@
+<?php
+    $nome = $_GET['nome'];
+    $email = $_GET['email'];
+    $dtNasc = $_GET['dataNasc'];
+    $cidade = $_GET['Cidade'];
+
+    echo "Olá $nome <br>";
+    echo "E-mail: $email<br>";
+    echo "Dt Nasc: $dtNasc<br>";
+    echo "Cidade: $cidade<br>";
+?>
